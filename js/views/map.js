882e8cc71d3f62e4404d28096940c3a976f09a8b
Joey Azoulai <joey@spotbrooklyn.com>
2014-08-19 13:58:32 -0400

    update mapbox id.

--- js/views/map.js
+++ js/views/map.js
@@ -8,7 +8,7 @@ sbk.MapView = Backbone.View.extend({
         this.spotCollection = collections.spotCollection;
         this.neighborhoodCollection = collections.neighborhoodCollection;
 
-        var lmap = this.lmap = L.mapbox.map('map', 'spotbrooklyn.3xa8n79b', {
+        var lmap = this.lmap = L.mapbox.map('map', 'spotbrooklyn.06i7wrk9q', {
             attributionControl: false
         });
 
