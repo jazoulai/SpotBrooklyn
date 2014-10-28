380dce6c817e0e7473ac68ae2515954b05439845
Joey Azoulai <joey@spotbrooklyn.com>
2014-10-28 14:30:21 -0400

    delegate followview events.

--- test/js/views/followView.js
+++ test/js/views/followView.js
@@ -5,15 +5,39 @@ sbk.FollowView = Backbone.View.extend({
 
 
     events: {
-        "click .twitter" : "alert"
+        "touchstart #follow_button" : "toggle_follow",
+        "touchstart .social_button" : "close_follow",
+        "touchstart #follow_twitter .social_button_img" : "follow_twitter",
+        "touchstart #follow_instagram .social_button_img" : "follow_instagram",
+        "touchstart #follow_email .social_button_img" : "follow_email"
     },
     template: Handlebars.compile($('#follow_template').html()),
     render: function (){
         $(this.el).html(this.template());
         return this;
     },
-    alert: function(){
-        alert('instagram!');
+    toggle_follow: function(){
+        $('#follow').animate({height: "10%"}, 500);
+        $('#follow').removeClass('hide');
+    },
+    close_follow: function () {
+        $('#follow').animate({height: "0%"}, 500);
+        //$('#follow').addClass('hide');
+    },
+    follow_twitter: function(){
+        var self = this;
+        window.open('https://twitter.com/intent/follow?screen_name=spotbrooklyn');
+        self.close_follow();
+    },
+    follow_instagram: function(){
+        var self = this;
+        window.open('http://instagram.com/spotbrooklyn?ref=badge');
+        self.close_follow();
+    },
+    follow_email: function(){
+        var self = this;
+        window.location.href = 'mailto:joey@spotbrooklyn.com';
+        self.close_follow();
     }
 
 });
