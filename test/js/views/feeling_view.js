82eb38f1eb47dbb13d35c2457e3dec56db83dbe5
Joey Azoulai <joey@spotbrooklyn.com>
2014-11-24 11:45:22 -0500

    shuffle feelings collection.

--- test/js/views/feeling_view.js
+++ test/js/views/feeling_view.js
@@ -2,10 +2,18 @@
 /*globals Backbone: false, L: false, $: false, Handlebars: false, _: false, sbk: false */
 
 sbk.FeelingView = Backbone.View.extend({
+    tagName: 'li',
     className: 'feeling',
     template: Handlebars.compile($('#feeling_template').html()),
     render: function () {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
+    },
+    events: {
+        'click' : 'navigate_feeling'
+    },
+    navigate_feeling : function(){
+        var feelingId = this.model.get('id');
+        sbk.app.navigate('!' + feelingId, {trigger: true});
     }
 });
--- test/js/views/feelings_view.js
+++ test/js/views/feelings_view.js
@@ -2,14 +2,18 @@
 /*globals Backbone: false, L: false, $: false, Handlebars: false, _: false, sbk: false */
 
 sbk.FeelingsView = Backbone.View.extend({
+    tagName: 'ul',
     id: 'feelings-list',
     initialize: function () {
-        this.collection.on("reset", this.render, this);
+        var filteredArray = this.collection.reset(this.collection.shuffle(), {silent:true});
+        this.filteredCollection = new Backbone.Collection(filteredArray);
+        this.collection.on('reset', this.render, this);
     },
     render: function () {
-        _.each(this.collection.models, function (story) {
+        _.each(this.filteredCollection.models, function (story) {
             $(this.el).append(new sbk.FeelingView({model: story}).render().el);
         }, this);
+
         return this;
     }
 });
