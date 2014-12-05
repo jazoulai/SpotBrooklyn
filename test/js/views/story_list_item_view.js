c6042e5a3686be4f22c4e87f122a322ef0f7f79c
Joey Azoulai <joey@spotbrooklyn.com>
2014-12-05 17:34:47 -0500

    update css layout and other styles.

--- test/js/views/story_list_item_view.js
+++ test/js/views/story_list_item_view.js
@@ -3,23 +3,19 @@
 
 sbk.StoryListItemView = Backbone.View.extend({
     tagName: 'li',
-    className: 'story-list-item-container',
+    className: 'story-list-item',
     template: Handlebars.compile($('#story-list-item-template').html()),
     render: function () {
         $(this.el).html(this.template(this.model.toJSON()));
         return this;
     },
     events : {
-        'click .like' : 'like',
-        'click .dislike' : 'dislike'
+        'click .plus-one' : 'plusOne'
     },
-    like: function () {
+    plusOne: function () {
         var title = this.model.get('id');
-        ga('send', 'event', 'dislike', 'click', title, 1);
+        ga('send', 'event', '+1', 'click', title, 1);
+        alert('+1');
 
-    },
-    dislike: function () {
-        var title = this.model.get('id');
-        ga('send', 'event', 'like', 'click', title, 1);
     }
 });
--- test/js/views/story_list_view.js
+++ test/js/views/story_list_view.js
@@ -5,15 +5,25 @@ sbk.StoryListView = Backbone.View.extend({
     tagName: 'ul',
     id: 'story-list',
     initialize: function () {
-        var filteredArray = this.collection.reset(this.collection.shuffle(), {silent:true});
-        this.filteredCollection = new Backbone.Collection(filteredArray);
+        this.filteredArray = this.collection.reset(this.collection.sample(4), {silent:true});
+        this.filteredCollection = new Backbone.Collection(this.filteredArray);
         this.collection.on('reset', this.render, this);
     },
+    template: Handlebars.compile($('#story-list-template').html()),
     render: function () {
         _.each(this.filteredCollection.models, function (storyItem) {
             $(this.el).append(new sbk.StoryListItemView({model: storyItem}).render().el);
         }, this);
 
+        $(this.el).append(this.template());
         return this;
+    },
+    events: {
+        'click span' : 'loadMore'
+    },
+    loadMore: function () {
+        $('html, body').animate({
+            scrollTop: $('#story-list').offset().top
+        }, 500);
     }
 });
