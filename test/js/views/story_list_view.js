4fba7eb4909fef133ef9db5815511fe1df97d11b
Joey Azoulai <joey@spotbrooklyn.com>
2014-12-12 19:07:31 -0500

    update copy.

--- test/js/views/story_list_view.js
+++ test/js/views/story_list_view.js
@@ -2,7 +2,7 @@
 /*globals Backbone: false, L: false, $: false, Handlebars: false, _: false, sbk: false */
 
 sbk.StoryListView = Backbone.View.extend({
-    tagName: 'ul',
+    tagName: 'div',
     id: 'story-list',
     initialize: function () {
         this.collection.on('reset', this.render, this);
@@ -12,7 +12,7 @@ sbk.StoryListView = Backbone.View.extend({
         this.newCollection = _.sample(this.collection.models, 3);
         $(this.el).html(this.template());
         _.each(this.newCollection, function (storyItem) {
-            $(this.el).find('#stories').prepend(new sbk.StoryListItemView({model: storyItem}).render().el);
+            $(this.el).find('#stories-list-ul').prepend(new sbk.StoryListItemView({model: storyItem}).render().el);
         }, this);
 
         return this;
@@ -22,15 +22,17 @@ sbk.StoryListView = Backbone.View.extend({
     },
     loadMore: function(ev){
 
-        if(this.collection.length > 2){
+        if(this.collection.length > 3){
             console.log(this.collection.length);
             var self = this;
             _.each(self.newCollection, function(child){
                 self.collection.remove(child);
             });
             this.render();
+            $('html, body').animate({
+                scrollTop: $('#story-list').offset().top
+            }, 500);
         } else {
-            console.log('results view');
             this.resultsSignupView = new sbk.ResultsSignupView();
             $(this.el).replaceWith(this.resultsSignupView.render().el);
             $('html, body').animate({
