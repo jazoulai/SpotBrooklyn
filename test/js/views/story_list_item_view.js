5ccbdd82c03cd5a2fa4f8b1d707d1a9ccf7026d4
Joey Azoulai <joey@spotbrooklyn.com>
2015-01-05 12:47:28 -0500

    rework copy, remove voting buttons, and style images.

--- test/js/views/story_list_item_view.js
+++ test/js/views/story_list_item_view.js
@@ -6,25 +6,10 @@ sbk.StoryListItemView = Backbone.View.extend({
     className: 'story-list-item',
     template: Handlebars.compile($('#story-list-item-template').html()),
     render: function () {
-        this.voteButtonsView = new sbk.VoteButtonsView();
         $(this.el).html(this.template(this.model.toJSON()));
-        $(this.el).append(this.voteButtonsView.render().el);
         return this;
     },
-    events: {
-        'click span' : 'vote'
-    },
     vote: function (ev) {
-        var target = $(ev.currentTarget);
-        var votePrompt = target.parent().siblings('.scrollLink');
-        target.toggleClass('select');
-        target.siblings().removeClass('select');
-
-        if(target.hasClass('select') || target.siblings().hasClass('select')) {
-            votePrompt.html('You voted!');
-        } else {
-            votePrompt.html('Click to Vote!');
-        }
         var vote = $(ev.currentTarget).attr('aria-label');
         var title = this.model.get('headline');
         ga('send', 'event', vote, 'click', title, 1);
--- test/js/views/story_list_view.js
+++ test/js/views/story_list_view.js
@@ -9,50 +9,17 @@ sbk.StoryListView = Backbone.View.extend({
     },
     template: Handlebars.compile($('#story-list-template').html()),
     sampleCollection: function() {
-        var self = this;
         this.storyList = $(this.el).find('#stories-list-ul');
-        this.sampledCollection = _.sample(this.collection.models, 3);
+        this.shuffledCollection = _.shuffle(this.collection.models);
 
         //append each story-list-item to the a child element of this view.
-        _.each(this.sampledCollection, function (idea) {
+        _.each(this.shuffledCollection, function (idea) {
             this.storyList.append(new sbk.StoryListItemView({model: idea}).render().el);
         }, this);
-
-        this.countListItems = this.storyList.find('li').length;
-        this.countTotalItems = this.collection.length;
-
-        //remove each child of the sampled copy from this.collection to avoid rendering duplicates.
-        _.each(self.sampledCollection, function(child){
-            self.collection.remove(child);
-        });
     },
     render: function () {
         $(this.el).html(this.template());
         this.sampleCollection();
-        //insert the total length of this.collection after the template is rendered
-        $(this.el).find('#tally').html(this.countListItems);
-        $(this.el).find('#count').html(' of ' + this.countTotalItems);
         return this;
-    },
-    events: {
-        'click #load-more' : 'loadMore'
-    },
-    loadMore: function(){
-        if(this.collection.length > 0){
-            this.sampleCollection();
-            $(this.el).find('#tally').html(this.countListItems);
-            var liIndex = this.countListItems - 4;
-
-            $('html, body').animate({
-             scrollTop: $('li:eq(' + liIndex + ')').offset().top
-             }, 500);
-
-            if (this.collection.length === 0) {
-                var button = $(this.el).find('button');
-                button.removeClass('enabled');
-                button.addClass('disabled');
-                button.html('No More');
-            }
-        }
     }
 });
